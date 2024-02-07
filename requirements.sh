82b4b9b42d8b7e64376e5bbe77d8dabc2dc5c7c5
mlem <vlad.ifju@gmail.com>
2024-02-08 01:05:44 +0200

    requirements shell script added

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the list of packages to install
+packages = ("picom" "i3" "ranger")
+
+# Update the package database
+sudo pacman -Sy
+
+# Install the specified packages
+sudo pacman -S --noconfirm "${packages[@]}"
+
