00f6e34040bd9f9a889da813dc6c0ceb743a0d10
mlem <vlad.ifju@gmail.com>
2024-02-08 19:05:20 +0200

    updated requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of packages to install
-packages = ("picom" "i3" "ranger")
+packages = ("picom" "i3" "ranger" "discord")
 
 # Update the package database
 sudo pacman -Sy
