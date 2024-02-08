82762223ad19f440f27ce725dab78d8fb347dac5
ml3m <vladutifju6@gmail.com>
2024-02-08 19:19:00 +0200

    updated requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of packages to install
-packages = ("picom" "i3" "ranger" "discord")
+packages = ("picom" "i3" "ranger" "discord" "firefox")
 
 # Update the package database
 sudo pacman -Sy
