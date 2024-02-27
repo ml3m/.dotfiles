1df319f2d7eccc5af98857203358831f1c6f3d1c
ml3m <vladutifju6@gmail.com>
2024-02-27 12:24:47 +0200

    up

--- configs/nvim/after/plugin/fugitive.lua
+++ configs/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,8 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git('push')
+end, opts)
+
+vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git({'pull',  '--rebase'})
+end, opts)
