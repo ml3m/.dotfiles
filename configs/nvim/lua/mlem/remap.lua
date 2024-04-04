67b6787adaba8cbf75e07311e70193612341d355
ml3m <vladutifju6@gmail.com>
2024-04-05 02:01:32 +0300

    chmod lua and tmux file spread

--- configs/nvim/lua/mlem/remap.lua
+++ configs/nvim/lua/mlem/remap.lua
@@ -21,3 +21,5 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- movement
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
