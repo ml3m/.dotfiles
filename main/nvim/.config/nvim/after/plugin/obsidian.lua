cec40a08969f43ceb4204990ac6394e72bb9d6c5
ml3m <vladutifju6@gmail.com>
2024-04-14 16:30:46 +0300

    MAIN

---
+++ main/nvim/.config/nvim/after/plugin/obsidian.lua
@@ -0,0 +1,10 @@
+local obsidian = require("obsidian");
+
+obsidian.setup({
+    workspaces = {
+        {
+            name = "uvt",
+            path = "~/personal/uvt/uvt/"
+        },
+    },
+})
